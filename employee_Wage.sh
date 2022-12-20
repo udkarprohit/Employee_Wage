bdace046e3cbbf98dd7d0f878c1f2452cd1f6516
Prohit <udkarprohit@gmail.com>
2022-12-21 00:48:01 +0530

    Added: Part time working employee and daily wages

--- employee_Wage.sh
+++ employee_Wage.sh
@@ -2,10 +2,11 @@
 
 Wage_Per_Hour=20;
 Working_Hour=8;
+Part_Time_Hour=4; 
 
 echo "Welcome to Employee wage computation";
 
-isPresent=$((RANDOM%2));
+isPresent=$((RANDOM%3));	#0 1 2
 
 if [ $isPresent -eq 0 ]
 then 
@@ -15,4 +16,8 @@ then
 	echo "Employee is Present";
 	dailyWage=$(($Wage_Per_Hour * $Working_Hour));
 	echo "PreDay Daily Wage is : "$dailyWage;
+else
+	echo "Employee is working as part time";
+	dWage=$(($Wage_Per_Hour * $Part_Time_Hour));
+	echo "Part time daily Wage is : "$dWage;
 fi

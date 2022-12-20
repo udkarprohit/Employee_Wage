988d46c8660d361e20b8027d14a103773ee30ef8
Prohit <udkarprohit@gmail.com>
2022-12-21 00:21:32 +0530

    Added: Employee is Present or Absent

--- employee_Wage.sh
+++ employee_Wage.sh
@@ -1,3 +1,12 @@
-#!/bin/bash
-
+ #!/bin/bash
 echo "Welcome to Employee wage computation";
+
+isPresent=$((RANDOM%2));
+
+if [ $isPresent -eq 0 ]
+then 
+	echo "Employee is Absent";
+elif [ $isPresent -eq 1 ]
+then
+	echo "Employee is Present";
+fi

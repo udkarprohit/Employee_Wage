bb97ec99ace632d35ec55ea0ca32e74f23eb08db
Prohit <udkarprohit@gmail.com>
2022-12-21 05:43:54 +0530

    Added: Calculating wages till a condition of total working hr or days is reached for a month

--- employee_Wage.sh
+++ employee_Wage.sh
@@ -1,30 +1,41 @@
  #!/bin/bashs
 
-isPresent=$((RANDOM%3));
+
+totalWage=0;
 Wage_Per_Hour=20;
 Working_Hour=0;
-Month=20;
+totalHour=0;
+day=1;
 #Part_Time_Hour=4; 
-
-case $isPresent in
-	0)
-		echo "Employee is Absent";
-		Working_Hour=0;
-	;;
+while [[ $day -le 20 && $totalHour -lt 100 ]]
+do
+	isPresent=$((RANDOM%3));
+	case $isPresent in
+		0)
+			#echo "Employee is Absent";
+			Working_Hour=0;
+		;;
 	
-	1)
-		echo "Employee is Present";
-		Working_Hour=8;
-	;;
+		1)
+			#echo "Employee is Present";
+			Working_Hour=8;
+		;;
 	
-	2)
-		echo "Employee is working as part time";
-		Working_Hour=4;
-	;;
-esac
+		2)
+			#echo "Employee is working as part time";
+			Working_Hour=4;
+		;;
+	esac
+	totalHour=$(($totalHour + $Working_Hour));
+	if [ $totalHour -gt 100 ]
+	then
+		totalHour=$(($totalHour - $Working_Hour));
+		break;
+	fi
+	dailyWage=$(($Wage_Per_Hour * $Working_Hour));
+	totalSalary=$(($totalSalary + $dailyWage));
+	#echo "Employee Daily Wage is : " $dailyWage;
+	((day++));
+done
 
-dailyWage=$(($Wage_Per_Hour * $Working_Hour));
-mWage=$(($dailyWage * $Month));
-
-echo "Employee Daily Wage is : " $dailyWage;
-echo "Employee Monthly Wage is : " $mWage;
+echo "Employee Monthly Wage is : "$totalSalary;
